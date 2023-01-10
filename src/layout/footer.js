d79d0bb2a4ace737dc95ce8f8c0dc445b091be83
Owusu Francis Buabin <codingfrancis100@gmail.com>
2023-01-10 23:49:46 +0000

    getting done with the footer

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -2,20 +2,42 @@ import React from 'react'
 import Logo from '../commons/logo'
 import Navbar from './navbar'
 import { Link } from 'react-router-dom'
+import { IoLogoWhatsapp } from 'react-icons/io'
+import { BsFacebook, BsTwitter, BsYoutube, BsPinterest } from 'react-icons/bs'
+import { AiFillInstagram } from 'react-icons/ai'
 const Footer = () => {
-  return (
-    <footer className='footer'>
-          <nav>
-              <Logo color='#fff' />
-              <div className='nav-links'>
-                  <Link className='nav-link link-light'>our company</Link>
-                  <Link className='nav-link link-light'>locations</Link>
-                  <Link className='nav-link link-light'>contact</Link>
-              </div>
-          </nav>
-          
-    </footer>
-  )
+    return (
+        <footer className='footer'>
+            <nav>
+                <Logo color='#fff' />
+                <div className='nav-links'>
+                    <Link className='nav-link link-light'>our company</Link>
+                    <Link className='nav-link link-light'>locations</Link>
+                    <Link className='nav-link link-light'>contact</Link>
+                </div>
+            </nav>
+            <div className='footer-details'>
+                <div className='location-details'>
+                    <p style={{fontWeight: 800}}>Designo Central Office</p>
+                    <p>3886 Wellington Street</p>
+                    <p>Toronto, Ontario M9C 3J5</p>
+                </div>
+                <div className='contact-details'>
+                    <p style={{fontWeight: 800}}>Contact Us (Central Office)</p>
+                    <p>P : +1 253-863-8967</p>
+                    <p>M : contact@designo.co</p>
+                </div>
+                <div className='social-media'>
+                    <IoLogoWhatsapp size={40}/>
+                    <BsFacebook size={40}/>
+                    <BsTwitter size={40}/>
+                    <BsYoutube size={40}/>
+                    <BsPinterest size={40}/>
+                    <AiFillInstagram size={40}/>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,6 +5,9 @@ import Navbar from '../layout/navbar'
 const Homepage = () => {
   return (
     <Layout>
+      <div style={{height: '100vh'}}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate optio perspiciatis debitis reiciendis quos voluptates quisquam sunt nesciunt quis velit laboriosam, in aliquam numquam veritatis iusto ipsum praesentium iste doloribus.
+      </div>
     </Layout>
   )
 }
